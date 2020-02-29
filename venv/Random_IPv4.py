118eaf6d704a939dd6b1eccbb0eb6db2fc6d98ad
ikev3 <magkh@qq.com>
2020-02-29 21:54:15 +0800

    2

---
+++ venv/Random_IPv4.py
@@ -0,0 +1,6 @@
+import random
+ip1 = random.randint(1,255)
+ip2 = random.randint(1,255)
+ip3 = random.randint(1,255)
+ip4 = random.randint(1,255)
+print(str(ip1)+'.'+ str(ip2)+'.'+ str(ip3)+'.'+ str(ip4))
